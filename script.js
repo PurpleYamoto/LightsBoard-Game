f2ea11c9b2bf66d3f3d6fac54b88a4de9c077729
Стас Асташенко <stasastasenko@192.168.1.33>
2022-05-07 13:26:06 +0300

    Script Update1.2

--- script.js
+++ script.js
@@ -8,4 +8,15 @@ for(let i = 0; i< SQUARES_NUMBER; i++){
     square.addEventListener('mouseover',()=> setColor(square));
     square.addEventListener('mouseleave',()=> removeColor(square));
     board.append(square);
-}
+};
+
+function setColor(element){
+    const color = getRandomColor();
+    element.style.background = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+};
+
+function removeColor(element){
+    element.style.background = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+};
