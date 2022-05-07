c8663d3e8383658a1ec46e2ebdfb99e56ab7b8a0
Стас Асташенко <stasastasenko@192.168.1.33>
2022-05-07 13:18:39 +0300

    Script Update1.1

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const board = document.querySelector('#board');
 const colors = ['red','blue','green','yellow','red','grey','pink','orange','purple']
 const SQUARES_NUMBER = 900;
+
+for(let i = 0; i< SQUARES_NUMBER; i++){
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.addEventListener('mouseover',()=> setColor(square));
+    square.addEventListener('mouseleave',()=> removeColor(square));
+    board.append(square);
+}
